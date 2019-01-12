5839207364f80ad7b4758cc10219c87575bcca0d
Majai <43511718+kit1997330@users.noreply.github.com>
2019-01-12 21:24:39 +0800

    updated login page (uncompleted)

--- app/src/main/java/com/example/fyp/Start.java
+++ app/src/main/java/com/example/fyp/Start.java
@@ -1,13 +1,29 @@
 package com.example.fyp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Start extends AppCompatActivity {
+
+    Button start_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        start_button = findViewById(R.id.start_button);
+
+        start_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v == start_button)  {
+                    startActivity(new Intent(Start.this, login.class));
+                }
+            }
+        });
     }
 }
