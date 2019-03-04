87a8471721c3bf49e465b1f78dcbab2661ff7914
Majai <43511718+kit1997330@users.noreply.github.com>
2019-03-04 22:24:01 +0800

    Updated What-If fragment

--- app/src/main/java/com/example/fyp/whatif_fragment.java
+++ app/src/main/java/com/example/fyp/whatif_fragment.java
@@ -7,8 +7,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class whatif_fragment extends Fragment {
+
+    SeekBar ReturnSeekBar, RiskSeekBar, SharpeSeekBar;
+    TextView ReturnSeekBarValue, RiskSeekBarValue, SharpeSeekBarValue;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -18,5 +23,68 @@ public class whatif_fragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        SeekBarStatus();
+    }
+
+    private void SeekBarStatus() {
+        ReturnSeekBar = getView().findViewById(R.id.ReturnSeekBar);
+        RiskSeekBar = getView().findViewById(R.id.RiskSeekBar);
+        SharpeSeekBar = getView().findViewById(R.id.ShapeRatioSeekBar);
+
+        ReturnSeekBarValue = getView().findViewById(R.id.ReturnSeekBarValue);
+        RiskSeekBarValue = getView().findViewById(R.id.RiskSeekBarValue);
+        SharpeSeekBarValue = getView().findViewById(R.id.ShapeRatioSeekBarValue);
+
+        ReturnSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                ReturnSeekBarValue.setText(progress+"/100");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        RiskSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                RiskSeekBarValue.setText(progress+"/100");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        SharpeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                SharpeSeekBarValue.setText(progress+"/100");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
