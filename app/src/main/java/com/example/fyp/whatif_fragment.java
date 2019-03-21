cf76655c0b50bb2a407ed20c8493ce580fe274b1
Majai <43511718+kit1997330@users.noreply.github.com>
2019-03-21 23:11:25 +0800

    updated what-if fragment

--- app/src/main/java/com/example/fyp/whatif_fragment.java
+++ app/src/main/java/com/example/fyp/whatif_fragment.java
@@ -7,18 +7,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class whatif_fragment extends Fragment {
 
-    /*SeekBar ReturnSeekBar, RiskSeekBar, SharpeSeekBar;
-    TextView ReturnSeekBarValue, RiskSeekBarValue, SharpeSeekBarValue;*/
+    EditText USStock1, USStock2, USStock3, USStock4, INStock1, INStock2, USBond1, USBond2, USBond3, INBond1, INBond2;
+    SeekBar RiskLevelSeekBar, ReturnLevelSeekBar;
+    TextView RiskLevel, ReturnLevel;
+    int x;
 
-    ListView USStockList, InternationalStockList, USBondsList, InternationalBondsList;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -29,90 +33,80 @@ public class whatif_fragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        USStockList = getView().findViewById(R.id.USStockList);
-        InternationalStockList = getView().findViewById(R.id.InternationalStockList);
-        USBondsList = getView().findViewById(R.id.USBondsList);
-        InternationalBondsList = getView().findViewById(R.id.InternationalBondsList);
-        LoadListItem();
-    }
+        USStock1 = getView().findViewById(R.id.USStockContent_1);
+        USStock2 = getView().findViewById(R.id.USStockContent_2);
+        USStock3 = getView().findViewById(R.id.USStockContent_3);
+        USStock4 = getView().findViewById(R.id.USStockContent_4);
 
-    public void LoadListItem() {
-        final US_Stock_Adapter s = new US_Stock_Adapter(getContext(), new ArrayList<US_Stock>());
-        final US_Bond_Adapter t = new US_Bond_Adapter(getContext(), new ArrayList<US_Bond>());
-        final Inter_Stock_Adapter u = new Inter_Stock_Adapter(getContext(), new ArrayList<Inter_Stock>());
-        final Inter_Bond_Adapter v = new Inter_Bond_Adapter(getContext(), new ArrayList<Inter_Bond>());
+        INStock1 = getView().findViewById(R.id.INStockContent_1);
+        INStock2 = getView().findViewById(R.id.INStockContent_2);
 
-        USStockList.setAdapter(s);
-        USBondsList.setAdapter(t);
-        InternationalStockList.setAdapter(u);
-        InternationalBondsList.setAdapter(v);
+        USBond1 = getView().findViewById(R.id.USBondContent_1);
+        USBond2 = getView().findViewById(R.id.USBondContent_2);
+        USBond3 = getView().findViewById(R.id.USBondContent_3);
 
-        TextView USStockPercentage = getView().findViewById(R.id.USStockPercentage);
-        TextView USBondsPercentage = getView().findViewById(R.id.USBondsPercentage);
-        TextView InternationalStockPercentage = getView().findViewById(R.id.InternationalStockPercentage);
-        TextView InternationalBondsPercentage = getView().findViewById(R.id.InternationalBondsPercentage);
+        INBond1 = getView().findViewById(R.id.INBondContent_1);
+        INBond2 = getView().findViewById(R.id.INBondContent_2);
 
+        RiskLevelSeekBar = getView().findViewById(R.id.OverallRiskSeekBar);
+        RiskLevelSeekBar.setProgress(40);
+        ReturnLevelSeekBar = getView().findViewById(R.id.OverallReturnSeekBar);
+        ReturnLevelSeekBar.setProgress(50);
 
-        int[] Percentage = new int[11];
-
-        String[] NameList = new String[11];
-
-        NameList[0] = "US Total Stock Market";
-        NameList[1] = "US Large-Cap Value";
-        NameList[2] = "US Mid-Cap Value";
-        NameList[3] = "US Small-Cap Value";
-        NameList[4] = "Intl. Developed Markets";
-        NameList[5] = "Intl. Emerging Markets";
-        NameList[6] = "US Inflation-Protected Bonds";
-        NameList[7] = "US High-Quality Bonds";
-        NameList[8] = "US Municipal Bonds";
-        NameList[9] = "Intl. Developed Market Bonds";
-        NameList[10] = "Intl. Emerging Market Bonds";
-
-        Percentage[0] = 65;
-        Percentage[1] = 5;
-        Percentage[2] = 5;
-        Percentage[3] = 5;
-        Percentage[4] = 0;
-        Percentage[5] = 0;
-        Percentage[6] = 0;
-        Percentage[7] = 20;
-        Percentage[8] = 16;
-        Percentage[9] = 10;
-        Percentage[10] = 10;
-
-        int sum1 = 0;
-        int sum2 = 0;
-        int sum3 = 0;
-        int sum4 = 0;
-
-        for (int count = 0; count <= 3; count++) {
-            sum1 = sum1 + Percentage[count];
-            US_Stock us_stock = new US_Stock(NameList[count], Integer.toString(Percentage[count])+"%");
-            s.add(us_stock);
+        RiskLevel = getView().findViewById(R.id.OverallRiskLevel);
+        x = RiskLevelSeekBar.getProgress();
+        if (x<=33) {
+            RiskLevel.setText("Less Risk");
+        } else if (x>=66) {
+            RiskLevel.setText("More Risk");
+        } else {
+            RiskLevel.setText("Moderate");
         }
-        USStockPercentage.setText(Integer.toString(sum1)+"%");
+        ReturnLevel = getView().findViewById(R.id.OverallReturnLevel);
+        x = ReturnLevelSeekBar.getProgress();
+        ReturnLevel.setText(Integer.toString(x));
 
-        for (int count = 4; count <= 5; count++) {
-            sum2 = sum2 + Percentage[count];
-            Inter_Stock inter_stock = new Inter_Stock(NameList[count], Integer.toString(Percentage[count])+"%");
-            u.add(inter_stock);
-        }
-        InternationalStockPercentage.setText(Integer.toString(sum2)+"%");
 
-        for (int count = 6; count <= 8; count++) {
-            sum3 = sum3 + Percentage[count];
-            US_Bond us_bond = new US_Bond(NameList[count], Integer.toString(Percentage[count])+"%");
-            t.add(us_bond);
-        }
-        USBondsPercentage.setText(Integer.toString(sum3)+"%");
+        RiskLevelSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (progress<=33) {
+                    RiskLevel.setText("Less Risk");
+                } else if (progress>=66) {
+                    RiskLevel.setText("More Risk");
+                } else {
+                    RiskLevel.setText("Moderate");
+                }
+            }
 
-        for (int count = 9; count <= 10; count++) {
-            sum4 = sum4 + Percentage[count];
-            Inter_Bond inter_bond = new Inter_Bond(NameList[count], Integer.toString(Percentage[count])+"%");
-            v.add(inter_bond);
-        }
-        InternationalBondsPercentage.setText(Integer.toString(sum4)+"%");
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        ReturnLevelSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Double s = Double.valueOf(progress);
+                ReturnLevel.setText(Double.toString(s/10));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
 
